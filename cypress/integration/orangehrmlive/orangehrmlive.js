b01645699dcc029f48bfd999a0e57c68f968c1ec
FAIZ FATHUR <faiz.fathur@investree.id>
2024-01-04 11:02:20 +0700

    Update Test Case

--- cypress/integration/orangehrmlive/orangehrmlive.js
+++ cypress/integration/orangehrmlive/orangehrmlive.js
@@ -1,8 +1,12 @@
 /// <reference types="cypress" />
 
-//import {faker} from '@faker-js/faker';
+// import { faker } from '@faker-js/faker';
 
 context('Automation Orange HRM Live', () => {
+
+    //const firstName = faker.person.firstName();
+    //const lastName = faker.person.lastName();
+
     beforeEach(() => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
         cy.get('[name="username"]').type('Admin')
@@ -17,18 +21,26 @@ context('Automation Orange HRM Live', () => {
         cy.get('.orangehrm-login-title').should('contain', 'Login')
     })
 
-    it.only('Update Personal Details',() => {
+    it('Update Personal Details',() => {
         cy.wait(3000)
         cy.get('.oxd-main-menu-item--name').contains('My Info').click()
         cy.get('.orangehrm-tabs-item').contains('Personal Details').click()
-        cy.get('[name="firstName"]').clear().type('Denias')
-        cy.get('[name="lastName"]').clear().type('Sadi')
-        //cy.get('.orangehrm-vertical-padding').children('.submit').click()
-        cy.get('[type="submit"]').parent('.oxd-form-actions').parent('.oxd-form').parent('.orangehrm-vertical-padding').contains('Save').click()
+        cy.get('[name="firstName"]').clear().type('Tono')
+        cy.get('[name="lastName"]').clear().type('Jono')
+        cy.get('.orangehrm-vertical-padding [type="submit"]').contains('Save').click()
+        //cy.get('[type="submit"]').parent('.oxd-form-actions').parent('.oxd-form').parent('.orangehrm-vertical-padding').contains('Save').click()
         cy.reload()
         cy.wait(3000)
-        cy.get('.oxd-userdropdown-name').should('contain', 'Denias Sadi')
+        cy.get('.oxd-userdropdown-name').should('contain', 'Tono Jono')
+    })
+
+    it('Update contact details',() => {
+        cy.wait(3000)
+        cy.get('.oxd-main-menu-item--name').contains('My Info').click()
+        cy.get('.orangehrm-tabs-item').contains('Contact Details').click()
+        cy.get('.oxd-label').contains('City').parent().siblings('div').find('input').clear().type('Bekasi')
+        cy.get('.oxd-form-actions [type="submit"]').contains('Save').click()
+        cy.get('.oxd-toast-content-text').should('contain', 'Successfully Updated')
     })
     
-
  })

f744962993e17b7fb87bf8ee4f280cb26644ba78
FAIZ FATHUR <faiz.fathur@investree.id>
2024-01-04 11:44:59 +0700

    update test case

--- cypress/integration/orangehrmlive/orangehrmlive.js
+++ cypress/integration/orangehrmlive/orangehrmlive.js
@@ -1,11 +1,12 @@
 /// <reference types="cypress" />
 
-// import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 
 context('Automation Orange HRM Live', () => {
 
-    //const firstName = faker.person.firstName();
-    //const lastName = faker.person.lastName();
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const city = faker.location.city();
 
     beforeEach(() => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
@@ -25,20 +26,20 @@ context('Automation Orange HRM Live', () => {
         cy.wait(3000)
         cy.get('.oxd-main-menu-item--name').contains('My Info').click()
         cy.get('.orangehrm-tabs-item').contains('Personal Details').click()
-        cy.get('[name="firstName"]').clear().type('Tono')
-        cy.get('[name="lastName"]').clear().type('Jono')
+        cy.get('[name="firstName"]').clear().type(firstName)
+        cy.get('[name="lastName"]').clear().type(lastName)
         cy.get('.orangehrm-vertical-padding [type="submit"]').contains('Save').click()
         //cy.get('[type="submit"]').parent('.oxd-form-actions').parent('.oxd-form').parent('.orangehrm-vertical-padding').contains('Save').click()
         cy.reload()
         cy.wait(3000)
-        cy.get('.oxd-userdropdown-name').should('contain', 'Tono Jono')
+        cy.get('.oxd-userdropdown-name').should('contain', firstName+' '+lastName)
     })
 
     it('Update contact details',() => {
         cy.wait(3000)
         cy.get('.oxd-main-menu-item--name').contains('My Info').click()
         cy.get('.orangehrm-tabs-item').contains('Contact Details').click()
-        cy.get('.oxd-label').contains('City').parent().siblings('div').find('input').clear().type('Bekasi')
+        cy.get('.oxd-label').contains('City').parent().siblings('div').find('input').clear().type(city)
         cy.get('.oxd-form-actions [type="submit"]').contains('Save').click()
         cy.get('.oxd-toast-content-text').should('contain', 'Successfully Updated')
     })
